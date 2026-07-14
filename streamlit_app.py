2e1f78ad703cc3607c90b26ceaa5e12987c557a8
vinicius270409 <vinicius270409@gmail.com>
2026-07-13 23:07:35 -0300

    Refactor text inputs and output messages

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,34 +2,34 @@ import streamlit as st
 
 st.set_page_config(page_title="Análise de Perfil", page_icon="👤")
 
-st.title("👤 Sistema de Análise de Desempenho Pessoal")
+st.title(" sistema de Análise de Desempenho Pessoal")
 
-nome = st.text_input("Nome do avaliado:")
-foco = st.radio("Qual sua maior habilidade hoje?", (
-    "Sumir do grupo do WhatsApp por dias", 
-    "Visualizar as mensagens e responder só na mente", 
-    "Fazer planos e desistir 2 horas depois",
-    "Manter o foco por no máximo 4 minutos"
+nome = st.text_input("nome do avaliado:")
+foco = st.radio("qual sua maior habilidade hoje?", (
+    "Ser Bumbum guloso maximmo", 
+    "Dar", 
+    "Receber",
+    "todos"
 ))
-rotina = st.text_input("Quantas horas produtivas você teve hoje?")
+rotina = st.text_input("quanto vc deu hoje")
 
 if nome and foco and rotina:
     st.write("---")
-    st.write(f"### ⚙️ Analisando dados de **{nome}**...")
+    st.write(f"###  analisando dados de bumbumzinho de **{nome}**...")
     
     try:
         horas = float(rotina)
         if horas <= 2:
             status_produtividade = "Praticamente um encosto. Fez absolutamente nada relevante."
         else:
-            status_produtividade = "Contando as horas que passou fingindo que estava fazendo algo útil."
+            status_produtividade = "Contando as horas que passou dando ne."
     except:
-        status_produtividade = "Incapaz de quantificar a própria rotina."
+        status_produtividade = "Bumbum guloso maquisimo"
 
-    st.error("**📋 DIAGNÓSTICO DO USUÁRIO:**")
+    st.error("**diagnostico:**")
     st.write(f"• **Comportamento Padrão:** {foco}.")
     st.write(f"• **Nível de Rendimento:** {status_produtividade}")
-    st.write(f"• **Conclusão:** Se o objetivo era a mediocridade, parabéns pelo excelente trabalho.")
+    st.write(f"• **Conclusão:** ta dano bem.")
 
     if st.button("Finalizar Relatório"):
-        st.info("Relatório gerado. Pare de procrastinar e responda quem te mandou esse link.")
+        st.info("Relatório gerado. parabens, bumbumzo gulosio.")
